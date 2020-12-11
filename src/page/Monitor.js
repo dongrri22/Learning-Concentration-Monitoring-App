934b98b3407da6efce421bccc8d3bd8a01d00f7b
usbin <rooprete99@naver.com>
2020-12-11 20:27:05 +0900

    js to react converting ...

--- src/page/Monitor.js
+++ src/page/Monitor.js
@@ -1,12 +1,9 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import './Monitor.css';
 import Axios from 'axios';
 import Webcam from 'webcam-easy';
-
-const MODEL_URL = '/models'
-// webcam.onloadedmetadata = () => {
-//     this.displaySize = { width:this.scrollWidth, height: this.scrollHeight }
-// };
+import $ from 'jquery';
+import * as faceapi from 'face-api.js';
 // const displayError=(err = '')=>{
 //     if(err!=''){
 //         document.getElementById("#errorMsg").html(err);
@@ -23,28 +20,9 @@ const MODEL_URL = '/models'
 //       document.getElementById(id).parent().addClass('disabled');
 //     }
 // }
-// const cameraStarted = ()=>{
-//     toggleContrl("detection-switch", true);
-//     document.getElementById('errorMsg').addClass("d-none");
-//     if( webcamGlobal.webcamList.length > 1){
-//         document.getElementById('#cameraFlip').removeClass('d-none');
-//     }
-// };
-// function cameraStopped(){
-//     toggleContrl("detection-switch", false);
-//     document.getElementById("#errorMsg").addClass("d-none");
-//    document.getElementById("#cameraFlip").addClass('d-none');
-// }
-// function toggleContrl(id, show){
-//     if(show){
-//         document.getElementById(id).prop('disabled', false);
-//         document.getElementById(id).parent().removeClass('disabled');
-//     }else{
-//         document.getElementById(id).prop('checked', false).change();
-//         document.getElementById(id).prop('disabled', true);
-//         document.getElementById(id).parent().addClass('disabled');
-//     }
-//   }
+
+
+
   
 //   class ErrorMsg extends Component {
 //       state = {
@@ -67,28 +45,74 @@ const MODEL_URL = '/models'
 //     }
 //     document.getElementById("errorMsg").removeClass("d-none");
 // }
+/**/
+//
+const modelPath = 'models';
+var webcam = {};
+var displaySize;
 
+function createCanvas(){
+    const webcamElement = document.getElementById('webcam');
+    if( document.getElementsByTagName("canvas").length == 0 )
+    {
+      var canvas = faceapi.createCanvasFromMedia(webcamElement)
+      document.getElementById('webcam-container').append(canvas)
+      faceapi.matchDimensions(canvas, displaySize)
+    }
+  }
+function cameraStarted(){
+    //detection-switch의 disabled를 false로
+    //detection-switch를 감싼 label에서 disabled class를 제거
+    document.getElementById('detection-switch').disabled = false;
+    document.getElementById('detectingSwitchLabel').className = 'form-switch';
+    
+    //errorMsg에 d-none 클래스 붙이기
+    //cameraFlip에서 d-none class를 제거
+    document.getElementById('errorMsg').className="col-12 alert-danger d-none";
+    if( webcam.webcamList.length > 1){
+        document.getElementById('cameraFlip').className = 'btn';
+    }
+};
+function cameraStopped(){
+    //detection-switch의 disabled를 true로
+    //detection-switch의 checked를 false로
+    //detection-switch를 감싼 label에 disabled class를 추가.
+    document.getElementById('detection-switch').disabled = true;
+    document.getElementById('detection-switch').checked= false;
+    document.getElementById('detectingSwitchLabel').className = "form-switch disabled";
+
+    //errorMsg에 d-none 클래스 추가
+    //cameraFlip에 d-none 클래스 추가
+    document.getElementById("errorMsg").className = "col-12 alert-danger d-none";
+    document.getElementById("cameraFlip").className = "btn d-none";
+}
 class WebcamSwitch extends Component{
     constructor(props){
         super(props);
-    }
-    state={
-        webcam : {}
+        this.state={
+            //webcam:{}
+        };
+        this.onClick = this.onClick.bind(this);
     }
     onClick(event){
         if(event.target.checked){
             console.log("check!");
             event.target.checked = true;
-            var webcamElement = document.getElementById('webcam')
-            this.webcam = new Webcam(webcamElement, 'user');
-            this.webcam.onloadedmetadata = function(e){
-                this.displaySize = { width:this.scrollWidth, height: this.scrollHeight }
+            var webcamElement = document.getElementById('webcam');
+            console.log(webcamElement);
+            webcam = new Webcam(webcamElement, 'user');
+            webcam.onloadedmetadata = function(e){
+                displaySize = { width:this.scrollWidth, height: this.scrollHeight }
             }
+            // this.setState({
+            //     webcam : new Webcam(webcamElement, 'user')
+            // });
+            
             console.log("webcam created.");
-            this.webcam.start()
+            webcam.start()
             .then(result =>{
-                //cameraStarted();
-                // webcamElement.style.transform = "";
+                cameraStarted();
+                webcamElement.style.transform = "";
                 // console.log("webcam started");
             })
             .catch(err => {
@@ -98,9 +122,9 @@ class WebcamSwitch extends Component{
         else {
             console.log("uncheck!");
             event.target.checked = false;     
-            // cameraStopped();
-            // webcam.stop();
-            // console.log("webcam stopped");
+            cameraStopped();
+            webcam.stop();
+            console.log("webcam stopped");
         }
     }
     render(){
@@ -109,16 +133,56 @@ class WebcamSwitch extends Component{
         )
     }
 }
-class Monitor extends Component{
+class DetectingSwitch extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            webcamGlobal : {}
+        this.state={
+            disabled : true
+        }
+        this.toggle = this.toggle.bind(this);
+    }
+    toggle(){
+        this.setState({
+            disabled : !this.state.disabled
+        })
+    }
+    onClick(event){
+        if(event.target.checked){
+            //loadingDetection의 class에서 d-none을 제거
+            document.getElementById('loadingDetection').className = "loading"
+            Promise.all([
+                faceapi.nets.ssdMobilenetv1.load(modelPath)
+            ]).then(function(){
+                createCanvas();
+                console.log("done!");
+                //startDetection();
+            })
         }
     }
-    getObject = (res) =>{
-        this.setState({ webcamGlobal : res }, () => {});
+    render(){
+        return(
+            <input type="checkbox" disabled={this.state.disabled} onChange={this.onClick} id="detection-switch"/>
+        )
     }
+}
+
+
+
+  
+class Monitor extends Component{
+    globals = {
+        webcamElement : {},//document.getElementById('webcam'),
+        webcam : {},//new Webcam(webcamElement, 'user'),
+        currentStream : {},
+        displaySize : {},
+        convas : {},
+        faceDetection : {}
+    }
+    
+    // getObject = (wE, w) =>{
+    //     this.setWebcamElement({ webcamElement : wE, webcam : new Webcam(webcamElement, 'user') }, () => {});
+
+    // }
     render(){
         return (
             <div className="container mt-1">
@@ -127,23 +191,23 @@ class Monitor extends Component{
                         <div className="row mb-3">
                             <div className="col-md-5 col-6 form-control">
                                 <label className="form-switch">
-                                    <WebcamSwitch webcam={this.webcamGlobal}/>
+                                    <WebcamSwitch/>
                                     <i></i>
                                     Start Camera
                                 </label>  
                                 <button id="cameraFlip" className="btn d-none"></button>
                             </div> 
                             <div className="col-md-5 col-6 form-control">
-                                <label className="form-switch disabled">
-                                    <input type="checkbox" disabled id="detection-switch"/>
-                                    <i></i>
-                                    Start Monitoring
+                                <label id='detectingSwitchLabel' className="form-switch disabled">
+                                <DetectingSwitch/>
+                                <i></i>
+                                Start Monitoring
                                 </label>      
                             </div>             
                         </div>
                     </div>
-                    <div className="col-12 col-md-10 col-xl-12 align-top" id="webcam-container">
-                        <div className="loading d-none">
+                    <div className="col-12 col-md-10 col-xl-10 align-top" id="webcam-container">
+                        <div id="loadingDetection" className="loading d-none">
                             Loading Model
                             <div className="spinner-border" role="status">
                                 <span className="sr-only"></span>
@@ -153,10 +217,14 @@ class Monitor extends Component{
                             <video id="webcam" autoPlay muted playsInline></video>
                             <canvas id="canvas" className="d-none" width='0' height='0'></canvas>
                         </div>  
-                        
+                        <div id="errorMsg" className="col-12 alert-danger d-none">
+                        Fail to start camera. Please allow permission to access camera.
+                         </div>
                     </div>
                 </div>
+                <script src='face-detection.js'></script>
             </div>
+            
         );
     }
 }
